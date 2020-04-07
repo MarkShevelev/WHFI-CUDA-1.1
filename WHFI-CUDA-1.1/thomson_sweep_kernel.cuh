05e0d15f83047ab0dfe3c24b513d0e5387a3ae73
Mark Shevelev <lionishy@gmail.com>
2020-04-07 16:20:16 +0300

    row count vs row index check omitted

--- WHFI-CUDA-1.1/thomson_sweep_kernel.cuh
+++ WHFI-CUDA-1.1/thomson_sweep_kernel.cuh
@@ -5,9 +5,8 @@
 
 namespace iki { namespace math { namespace device { 
 	template <typename T>
-	__global__ void thomson_sweep_kernel(T *a, T *b, T *c, T *d, T *x, unsigned row_size, unsigned stride = 1, unsigned rows_count = 1) {
-		unsigned row_id = threadIdx.x+ blockIdx.x * blockDim.x;
-		if (row_id >= rows_count) return;
+	__global__ void thomson_sweep_kernel(T *a, T *b, T *c, T *d, T *x, unsigned row_size, unsigned stride = 1) {
+		unsigned row_id = threadIdx.x + blockIdx.x * blockDim.x;
 		thomson_sweep(a + row_id, b + row_id, c + row_id, d + row_id, x + row_id, row_size, stride);
 	}
 }/*device*/ }/*math*/ }/*iki*/
