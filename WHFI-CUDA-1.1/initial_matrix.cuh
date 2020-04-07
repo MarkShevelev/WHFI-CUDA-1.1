ed34b6dbf204ed1f4158ef4d9d0367d20ec2bf94
Mark Shevelev <lionishy@gmail.com>
2020-04-07 15:57:17 +0300

    initial matrix refactored to init strided arrays

--- WHFI-CUDA-1.1/initial_matrix.cuh
+++ WHFI-CUDA-1.1/initial_matrix.cuh
@@ -3,12 +3,12 @@
 template <typename T>
 __device__ void initial_matrix(T *a, T *b, T *c, T *d, T *x, unsigned size, unsigned stride, T ans) {
 	for (unsigned offset = 0; offset != size; ++offset) {
-		a[offset] = 1;
-		b[offset] = 3;
-		c[offset] = 1;
-		d[offset] = 5 * ans;
-		x[offset] = 0;
+		a[offset * stride] = 1;
+		b[offset * stride] = 3;
+		c[offset * stride] = 1;
+		d[offset * stride] = 5 * ans;
+		x[offset * stride] = 0;
 	}
 
-	d[0] = d[size - 1] = 4 * ans;
+	d[0] = d[stride * (size - 1)] = 4 * ans;
 }
