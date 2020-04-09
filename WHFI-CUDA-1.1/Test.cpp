2066cdb822c2d2615c3f6e3fe26697bb5c38e375
Mark Shevelev <lionishy@gmail.com>
2020-04-09 13:03:49 +0300

    IO test completed

--- WHFI-CUDA-1.1/Test.cpp
+++ WHFI-CUDA-1.1/Test.cpp
@@ -1,30 +1,34 @@
 #include "DataTable.h"
+#include "DataTableIO.h"
 
 #include <iostream>
+#include <algorithm>
 
 int main() {
 	using namespace std;
 	using namespace iki;
 
-	DataTable<char, 2u, 1u> vdf_table(iki::Bounds<2u>({ 10,10 }));
-	//TO DO: get_bounds()
+	{
+		DataTable<char, 2u, 1u> table(iki::Bounds<2u>({ 10,10 }));
+		auto &bounds = table.get_bounds();
+		auto idx = bounds.first();
+		for (size_t scalar_idx = 0; scalar_idx != bounds.size(); ++scalar_idx) {
+			*table[scalar_idx].begin() = 'a' + idx[0];
+			bounds.next(idx);
+		}
 
-	auto &bounds = vdf_table.get_bounds();
-	auto idx = bounds.first();
-	for (size_t scalar_idx = 0; scalar_idx != bounds.size(); ++scalar_idx) {
-		*vdf_table[scalar_idx].begin() = 'a' + idx[0];
-		bounds.next(idx);
+		cout << table << endl;
 	}
 
-	auto ptr = vdf_table.raw_data();
-	for (unsigned row_id = 0; row_id != 10; ++row_id) {
-		for (unsigned elm_id = 0; elm_id != 10; ++elm_id)
-			cout << ptr[elm_id + row_id * 10] << ' ';
-		cout << endl;
-	}
-
-	for (auto idx = bounds.first(); idx != bounds.last(); bounds.next(idx)) {
-		cout << idx[0] << ' ' << idx[1] << '\n';
+	{
+		DataTable<char, 2u, 2u> scale_table(iki::Bounds<2u>({ 10,10 }));
+		auto &bounds = scale_table.get_bounds();
+		auto idx = bounds.first();
+		for (size_t scalar_idx = 0; scalar_idx != bounds.size(); ++scalar_idx) {
+			std::fill(scale_table[scalar_idx].begin(), scale_table[scalar_idx].end(), 'a' + idx[0]);
+			bounds.next(idx);
+		}
+		cout << scale_table;
 	}
 
 
