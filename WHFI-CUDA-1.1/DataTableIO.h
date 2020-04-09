221bee60815f8f25a274cf8bf05598f22ea7d905
Mark Shevelev <lionishy@gmail.com>
2020-04-09 12:52:56 +0300

    data table io initiated

---
+++ WHFI-CUDA-1.1/DataTableIO.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "DataTable.h"
+
+#include <iostream>
+#include <algorithm>
+
+template <typename It>
+std::ostream &operator<<(std::ostream &ascii_out, iki::Range<It> range) {
+	for (auto &x : range)
+		ascii_out << x << ' ';
+}
+
+template <typename T, unsigned Dim, unsigned Scale>
+std::ostream &operator<<(std::ostream &ascii_out, iki::DataTable<T, Dim, Scale> const &table) {
+	auto vector_idx = table.get_bounds().first();
+	for (size_t scalar_idx = 0; scalar_idx != table.get_bounds().size(); ++scalar_idx, table.get_bounds().next(vector_idx)) {
+		ascii_out << table[scalar_idx] << '\n';
+	}
+}
+
+namespace iki {
+
+}/*iki*/
