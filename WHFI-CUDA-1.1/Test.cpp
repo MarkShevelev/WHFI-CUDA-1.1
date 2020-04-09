07422cb49520461720bc9e27fa0f4d11a2db04fd
Mark Shevelev <lionishy@gmail.com>
2020-04-09 11:45:37 +0300

    read and write table test

--- WHFI-CUDA-1.1/Test.cpp
+++ WHFI-CUDA-1.1/Test.cpp
@@ -1,10 +1,27 @@
 #include "DataTable.h"
 
+#include <iostream>
+
 int main() {
 	using namespace std;
 	using namespace iki;
 
-	DataTable<float, 2u, 1u> vdf_table;
+	DataTable<char, 2u, 1u> vdf_table(iki::Bounds<2u>({ 10,10 }));
+	//TO DO: get_bounds()
+
+	iki::Bounds<2u> bounds = { 10,10 };
+	auto idx = bounds.first();
+	for (size_t scalar_idx = 0; scalar_idx != bounds.size(); ++scalar_idx) {
+		*vdf_table[scalar_idx].begin() = 'a' + idx[0];
+		bounds.next(idx);
+	}
+
+	auto ptr = vdf_table.raw_data();
+	for (unsigned row_id = 0; row_id != 10; ++row_id) {
+		for (unsigned elm_id = 0; elm_id != 10; ++elm_id)
+			cout << ptr[elm_id + row_id * 10] << ' ';
+		cout << endl;
+	}
 
 
 	return 0;
