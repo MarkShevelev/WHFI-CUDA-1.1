bd627075d9ed3c221f5d26520682970ba2e034e0
Mark Shevelev <lionishy@gmail.com>
2020-04-09 23:14:57 +0300

    bug with bounds size fixed

--- WHFI-CUDA-1.1/DataTableIO.h
+++ WHFI-CUDA-1.1/DataTableIO.h
@@ -15,8 +15,8 @@ std::ostream &operator<<(std::ostream &ascii_out, iki::Range<It> range) {
 
 template <typename T, unsigned Dim, unsigned Scale>
 std::ostream &operator<<(std::ostream &ascii_out, iki::DataTable<T, Dim, Scale> const &table) {
-	auto vector_idx = first(table.get_bounds());
-	for (size_t scalar_idx = 0; scalar_idx != size(table.get_bounds()); ++scalar_idx, next(vector_idx,table.get_bounds())) {
+	auto vector_idx = iki::first(table.get_bounds());
+	for (size_t scalar_idx = 0; scalar_idx != iki::size(table.get_bounds()); ++scalar_idx, iki::next(vector_idx,table.get_bounds())) {
 		ascii_out << vector_idx << ' ' << table[scalar_idx] << '\n';
 	}
 	return ascii_out;
