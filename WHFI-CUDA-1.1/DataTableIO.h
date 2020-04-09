b766027a288e88d971006bf5810ead7804415023
Mark Shevelev <lionishy@gmail.com>
2020-04-09 13:08:55 +0300

    index output with table

--- WHFI-CUDA-1.1/DataTableIO.h
+++ WHFI-CUDA-1.1/DataTableIO.h
@@ -16,7 +16,7 @@ template <typename T, unsigned Dim, unsigned Scale>
 std::ostream &operator<<(std::ostream &ascii_out, iki::DataTable<T, Dim, Scale> const &table) {
 	auto vector_idx = table.get_bounds().first();
 	for (size_t scalar_idx = 0; scalar_idx != table.get_bounds().size(); ++scalar_idx, table.get_bounds().next(vector_idx)) {
-		ascii_out << table[scalar_idx] << '\n';
+		ascii_out << vector_idx << ' ' << table[scalar_idx] << '\n';
 	}
 	return ascii_out;
 }
