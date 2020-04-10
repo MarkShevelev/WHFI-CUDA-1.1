832f9f8e95491e93d36c4c4966ab9b0272977c38
Mark Shevelev <lionishy@gmail.com>
2020-04-10 18:44:17 +0300

    grid to combine axis and table

--- WHFI-CUDA-1.1/UniformGrid.h
+++ WHFI-CUDA-1.1/UniformGrid.h
@@ -3,13 +3,30 @@
 #include "DataTable.h"
 #include "UniformSpace.h"
 
-namespace iki {
+#include <algorithm>
+
+namespace iki { namespace grid {
+	template <typename T>
+	struct Axis final {
+		T begin = { 0 }, step = { 0 };
+	};
+
+	template <typename T, unsigned Dim>
+	using UniformSpace = std::array<Axis<T>, Dim>;
+
+	template <typename T, unsigned Dim>
+	using Argument = std::array<T, Dim>;
+
+	template <typename T, unsigned Dim>
+	Argument<T,Dim> make_argument(table::Index<Dim> const &vector_idx, UniformSpace<T,Dim> const &space) {
+		Argument<T, Dim> vector_argument;
+		std::transform(vector_idx.begin(), vector_idx.end(), space.begin(), space.end(), vector_argument.begin(), [] (auto idx, auto axis) { return axis.begin + axis.step * idx; });
+		return vector_argument;
+	}
+
 	template <typename T, unsigned Dim, unsigned Scale>
 	struct UniformGrid final {
-
-
-	private:
 		UniformSpace<T, Dim> space;
-		DataTable<T, Dim, Scale> table;
+		table::DataTable<T, Dim, Scale> table;
 	};
-}/*iki*/
+}/*grid*/ }/*iki*/
