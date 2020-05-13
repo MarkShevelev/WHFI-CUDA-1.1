755f9d90b9fabdedc1bfec6610fe08bdebf975b5
Mark Shevelev <lionishy@gmail.com>
2020-05-13 16:36:05 +0300

    Test done

--- WHFI-CUDA-1.1/Test.cu
+++ WHFI-CUDA-1.1/Test.cu
@@ -1,4 +1,4 @@
-#include "OneDimensionalMultithreadDiffusion.cuh"
+#include "TwoDimensionalMultithreadDiffusion.cuh"
 #include "Device.h"
 #include "DeviceMemory.h"
 #include "DataTable.h"
@@ -21,19 +21,27 @@ int main() {
 	try {
 		Bounds<2u> v_field_size = { 256, 512 };
 		UniformSpace<float, 2u> v_space = { Axis<float>{0., 1.e-3f}, Axis<float>{-15.f, 1.e-3f} };
+
+		Bounds<2u> transposed_field_size = { v_field_size[1], v_field_size[0] };
+		UniformSpace<float, 2u> transposed_space = { Axis<float>{-15.f, 1.e-3f}, Axis<float>{0., 1.e-3f} };
+		
 		UniformGrid<float, 2u, 1u> v_field(v_space,v_field_size);
-		UniformGrid<float, 2u, 1u> along_dfc_field(v_space, v_field_size);
-		UniformGrid<float, 2u, 1u> perp_dfc_field(v_space, v_field_size);
 		v_field_init(v_field);
+
+		UniformGrid<float, 2u, 1u> along_dfc_field(v_space, v_field_size);
 		along_dfc_field_init(along_dfc_field);
+
+		UniformGrid<float, 2u, 1u> perp_dfc_field(transposed_space,transposed_field_size);
+		perp_dfc_field_init(perp_dfc_field);
 
 		Device device(0);
 
 		unsigned const row_size = v_field_size[1], row_count = v_field_size[0], field_size = row_size*row_count;
-		DeviceMemory dev_memory(field_size * 8 * sizeof(float));
-		float *x_next = (float *)dev_memory;
-		float *x_prev = x_next + field_size;
-		float *along_dfc = x_prev + field_size;
+		DeviceMemory dev_memory(field_size * 9 * sizeof(float));
+		float *x_prev = (float *)dev_memory;
+		float *x_next = x_prev + field_size;
+		float *x_tmp = x_next + field_size;
+		float *along_dfc = x_tmp + field_size;
 		float *perp_dfc = along_dfc + field_size;
 		float *a = perp_dfc + field_size;
 		float *b = a + field_size;
@@ -43,9 +51,9 @@ int main() {
 		device::to_device(x_prev, v_field.table);
 		device::to_device(x_next, v_field.table);
 		device::to_device(along_dfc, along_dfc_field.table);
-		device::to_device(perp_dfc, along_dfc_field.table);
+		device::to_device(perp_dfc, perp_dfc_field.table);
 
-		diffusion::OneDimensionalMultithreadDiffusion<32u,256u,float> diffusion_solver(1.f, row_size, row_count, x_next, x_prev, along_dfc, a, b, c, d);
+		diffusion::TwoDimensionalMultithreadDiffusion<32u, 256u, float> diffusion_solver(row_count, row_size, a, b, c, d, x_prev, x_next, x_tmp, along_dfc, 1.0f, perp_dfc, 1.0f);
 		for (unsigned iter_cnt = 0; iter_cnt != 1000; ++iter_cnt)
 			diffusion_solver.step();
 
@@ -78,11 +86,20 @@ void v_field_init(iki::grid::UniformGrid<float, 2u, 1u> &v_field) {
 
 }
 
-void dfc_field_init(iki::grid::UniformGrid<float, 2u, 1u> &dfc_field) {
+void along_dfc_field_init(iki::grid::UniformGrid<float, 2u, 1u> &dfc_field) {
 	auto &table = dfc_field.table;
 	auto &bounds = table.get_bounds();
 
-	for (unsigned row_idx = 1; row_idx != bounds[0]-2; ++row_idx)
-		for (unsigned elm_idx = 0; elm_idx != bounds[1]-1; ++elm_idx)
+	for (unsigned row_idx = 0; row_idx != bounds[0]; ++row_idx)
+		for (unsigned elm_idx = 0; elm_idx != bounds[1]; ++elm_idx)
+			*(table[row_idx + elm_idx * bounds[0]].begin()) = 1.f;
+}
+
+void perp_dfc_field_init(iki::grid::UniformGrid<float, 2u, 1u> &dfc_field) {
+	auto &table = dfc_field.table;
+	auto &bounds = table.get_bounds();
+
+	for (unsigned row_idx = 1; row_idx != bounds[0] - 2; ++row_idx)
+		for (unsigned elm_idx = 1; elm_idx != bounds[1] - 2; ++elm_idx)
 			*(table[row_idx + elm_idx * bounds[0]].begin()) = 1.f;
 }
