d4b84b3120159a74c5d441c662c1457c6b5e6fc6
Mark Shevelev <lionishy@gmail.com>
2020-05-13 16:34:19 +0300

    diagonal discretization

--- WHFI-CUDA-1.1/diagonal_discretization.cuh
+++ WHFI-CUDA-1.1/diagonal_discretization.cuh
@@ -5,7 +5,7 @@
 namespace iki { namespace diffusion { namespace device {
 	template <typename T>
 	inline
-	__device__ T diagonal_discretization(T const *x, T const *dfc, unsigned idx, unsigned stride  = 1) {
-		return fma(dfc[idx], x[idx + stride], -dfc[idx] * x[idx]) + fma(dfc[idx - stride], x[idx - stride], -dfc[idx - stride] * x[idx]);
+	__device__ T diagonal_discretization(T x_im1_j, T x_i_j, T x_ip1_j, T d_im1_j, T d_i_j) {
+		return d_i_j * (x_ip1_j - x_i_j) - d_im1_j * (x_i_j - x_im1_j);
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
