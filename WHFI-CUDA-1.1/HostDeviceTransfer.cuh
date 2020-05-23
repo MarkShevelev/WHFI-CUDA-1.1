4ada5de97c3b001f64cab6b76108b0237132c345
Mark Shevelev <lionishy@gmail.com>
2020-05-23 09:22:22 +0300

    data transfer from and to device added

---
+++ WHFI-CUDA-1.1/HostDeviceTransfer.cuh
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "HostTable.h"
+#include "HostManagedDeviceTable.cuh"
+#include "DeviceError.h"
+
+#include <cuda_runtime.h>
+
+namespace iki { namespace table { namespace test { 
+	template <typename T>
+	void host_to_device_transfer(HostTable<T> const &host, HostManagedDeviceTable<T> &device) {
+		cudaError_t cudaStatus = cudaMemcpy(device.dMemory, host.hData.data(), host.full_size() * sizeof(T), cudaMemcpyHostToDevice);
+		if (cudaSuccess != cudaStatus)
+			throw DeviceError("Host to device data transfer failed: ", cudaStatus);
+	}
+
+	template <typename T>
+	void device_to_host_transfer(HostManagedDeviceTable<T> const &device, HostTable<T> &host) {
+		cudaError_t cudaStatus = cudaMemcpy(host.hData.data(), device.dMemory, device.full_size() * sizeof(T), cudaMemcpyDeviceToHost);
+		if (cudaSuccess != cudaStatus)
+			throw DeviceError("Host to device data transfer failed: ", cudaStatus);
+	}
+}/*test*/ }/*table*/ }/*iki*/
