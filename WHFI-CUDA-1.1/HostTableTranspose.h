af2c78b688c45f60600438cd98dd3117642b1e8a
Mark Shevelev <lionishy@gmail.com>
2020-05-23 13:39:23 +0300

    host and device transpose function added

---
+++ WHFI-CUDA-1.1/HostTableTranspose.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "HostTable.h"
+
+namespace iki { namespace table { namespace test {
+	template <typename T>
+	void transpose(HostTable<T> const &from, HostTable<T> &to) {
+		to.swap_sizes();
+		for (unsigned row_idx = 0; row_idx != from.row_count; ++row_idx)
+			for (unsigned elm_idx = 0; elm_idx != from.row_size; ++elm_idx)
+				to(elm_idx, row_idx) = from(row_idx, elm_idx);
+	}
+}/*test*/ }/*table*/ }/*iki*/
