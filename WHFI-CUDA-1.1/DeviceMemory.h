29ee53d4275a2c6f457e74c4b07a45d41f836f48
Mark Shevelev <lionishy@gmail.com>
2020-05-23 09:23:08 +0300

    device memory updated for implicit conversion to the void pointer

--- WHFI-CUDA-1.1/DeviceMemory.h
+++ WHFI-CUDA-1.1/DeviceMemory.h
@@ -17,6 +17,14 @@ namespace iki {
 		void *get_pointer() const;
 		unsigned get_size() const;
 
+		operator void const *() const {
+			return device_ptr;
+		}
+
+		operator void *() {
+			return device_ptr;
+		}
+
 		template <typename T>
 		explicit operator T* () const { return reinterpret_cast<T *>(device_ptr); }
 
