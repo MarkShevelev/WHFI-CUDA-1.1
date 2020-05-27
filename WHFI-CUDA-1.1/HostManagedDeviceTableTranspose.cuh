e0d0be033d475d9a9137aeb67d149aecc84934b9
Mark Shevelev <lionishy@gmail.com>
2020-05-27 10:19:31 +0300

    size swap with transposition canceld and cycle transposed removed as no longer need

--- WHFI-CUDA-1.1/HostManagedDeviceTableTranspose.cuh
+++ WHFI-CUDA-1.1/HostManagedDeviceTableTranspose.cuh
@@ -8,19 +8,11 @@
 
 namespace iki { namespace table { namespace test {
 	template <typename T>
-	void transope(HostManagedDeviceTable<T> const &from, HostManagedDeviceTable<T> &to) {
+	void transpose(HostManagedDeviceTable<T> const &from, HostManagedDeviceTable<T> &to) {
 		cudaError_t cudaStatus;
-
-		dim3 grid(from.row_count / 32, from.row_size / 32), threads(32, 8);
+		dim3 grid(from.dTable.row_count / 32, from.dTable.row_size / 32), threads(32, 8);
 		math::device::transpose_kernell<32,8><<<grid,threads>>> (to.data(), from.data(), from.dTable.row_count, from.dTable.row_size);
 		if (cudaSuccess != (cudaStatus = cudaGetLastError()))
 			throw DeviceError("Can't transpose: ", cudaStatus);
-		to.swap_sizes();
-	}
-
-	template <typename T>
-	void cycle_transpose(HostManagedDeviceTable<T> &t1, HostManagedDeviceTable<T> &t2, HostManagedDeviceTable<T> &t3) { //t1->t2->t3
-		transpose(t2, t3);
-		transpose(t1, t3);
 	}
 }/*test*/ }/*table*/ }/*iki*/
