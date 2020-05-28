12891dec5a6cf23d2671f0560a596371d466c933
Mark Shevelev <lionishy@gmail.com>
2020-05-28 09:53:58 +0300

    DeviceTable should be passed by value

--- WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "DeviceTable.cuh"
 #include "diagonal_discretization.cuh"
 
 #include <cuda_runtime.h>
@@ -7,18 +8,21 @@
 
 namespace iki { namespace diffusion{ namespace device {
 	template <unsigned TILE_SIZE, typename T>
-	__global__ void  forward_step_matrix_calculation_kernel(unsigned row_count, unsigned row_size, T *a, T *b, T *c, T *d, T const *x_curr, T const *along_dfc, T along_r, T const *perp_dfc, T perp_r) {
+	__global__ void  forward_step_matrix_calculation_kernel(
+		table::DeviceTable<T> a, table::DeviceTable<T> b, table::DeviceTable<T> c, table::DeviceTable<T> d,
+		table::DeviceTable<T> const x_curr, 
+		table::DeviceTable<T> const along_dfc, T along_r,
+		table::DeviceTable<T> const perp_dfc, T perp_r) {
 		unsigned row_idx = blockIdx.x * TILE_SIZE + threadIdx.x;
 		unsigned elm_idx = blockIdx.y * TILE_SIZE + threadIdx.y;
-		unsigned idx = row_idx + elm_idx * row_count;
 
-		if (0 == row_idx || row_count - 1 == row_idx || 0 == elm_idx || row_size - 1 == elm_idx) return;
+		if (0 == row_idx || x_curr.row_count - 1 == row_idx || 0 == elm_idx || x_curr.row_size - 1 == elm_idx) return;
 
-		a[idx] = -0.5 * along_r * along_dfc[idx - row_count];
-		b[idx] = T(1) + 0.5 * along_r * (along_dfc[idx - row_count] + along_dfc[idx]);
-		c[idx] = -0.5 * along_r * along_dfc[idx];
-		d[idx] = x_curr[idx] 
-			+ 0.5 * along_r * diagonal_discretization(x_curr[idx - row_count], x_curr[idx], x_curr[idx + row_count], along_dfc[idx - row_count], along_dfc[idx])
-			+ perp_r * diagonal_discretization(x_curr[idx - 1], x_curr[idx], x_curr[idx + 1], perp_dfc[elm_idx + (row_idx - 1) * row_size], perp_dfc[elm_idx + row_idx * row_size]);
+		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx-1);
+		b(row_idx, elm_idx) = T(1) + 0.5 * along_r * (along_dfc(row_idx, elm_idx-1) + along_dfc(row_idx, elm_idx));
+		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx);
+		d(row_idx, elm_idx) = x_curr(row_idx, elm_idx)
+			+ 0.5 * along_r * diagonal_discretization(x_curr(row_idx, elm_idx-1), x_curr(row_idx, elm_idx), x_curr(row_idx, elm_idx+1), along_dfc(row_idx, elm_idx-1), along_dfc(row_idx, elm_idx))
+			+ perp_r * diagonal_discretization(x_curr(row_idx-1, elm_idx), x_curr(row_idx, elm_idx), x_curr(row_idx+1, elm_idx), perp_dfc(elm_idx, row_idx-1), perp_dfc(elm_idx, row_idx));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
