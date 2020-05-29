d8866d38b8159a14487ba809178a90c2796d3326
Mark Shevelev <lionishy@gmail.com>
2020-05-29 17:11:17 +0300

    correction and forward steps been rewritten

--- WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
@@ -2,6 +2,7 @@
 
 #include "DeviceTable.cuh"
 #include "diagonal_discretization.cuh"
+#include "mixed_term_discretization.cuh"
 
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -10,9 +11,11 @@ namespace iki { namespace diffusion{ namespace device {
 	template <unsigned TILE_SIZE, typename T>
 	__global__ void  forward_step_matrix_calculation_kernel(
 		table::device::DeviceTable<T> a, table::device::DeviceTable<T> b, table::device::DeviceTable<T> c, table::device::DeviceTable<T> d,
-		table::device::DeviceTable<T> const x_curr, 
+		table::device::DeviceTable<T> const x_curr,
 		table::device::DeviceTable<T> const along_dfc, T along_r,
-		table::device::DeviceTable<T> const perp_dfc, T perp_r) {
+		table::device::DeviceTable<T> const perp_dfc, T perp_r,
+		table::device::DeviceTable<T> const along_mixed_dfc, table::device::DeviceTable<T> const perp_mixed_dfc, T mixed_r
+	) {
 		unsigned row_idx = blockIdx.x * TILE_SIZE + threadIdx.x;
 		unsigned elm_idx = blockIdx.y * TILE_SIZE + threadIdx.y;
 
@@ -22,7 +25,9 @@ namespace iki { namespace diffusion{ namespace device {
 		b(row_idx, elm_idx) = T(1) + 0.5 * along_r * (along_dfc(row_idx, elm_idx-1) + along_dfc(row_idx, elm_idx));
 		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx);
 		d(row_idx, elm_idx) = x_curr(row_idx, elm_idx)
-			+ 0.5 * along_r * diagonal_discretization(x_curr(row_idx, elm_idx-1), x_curr(row_idx, elm_idx), x_curr(row_idx, elm_idx+1), along_dfc(row_idx, elm_idx-1), along_dfc(row_idx, elm_idx))
-			+ perp_r * diagonal_discretization(x_curr(row_idx-1, elm_idx), x_curr(row_idx, elm_idx), x_curr(row_idx+1, elm_idx), perp_dfc(elm_idx, row_idx-1), perp_dfc(elm_idx, row_idx));
+			+ 0.5 * along_r * along_diagonal_discretization(x_curr,along_dfc,row_idx,elm_idx)
+			+ perp_r * perp_diagonal_discretization(x_curr, perp_dfc, row_idx, elm_idx)
+			+ mixed_r * along_mixed_term_discretization(x_curr,along_mixed_dfc,row_idx,elm_idx)
+			+ mixed_r * perp_mixed_term_discretization(x_curr,perp_mixed_dfc,row_idx,elm_idx);
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
