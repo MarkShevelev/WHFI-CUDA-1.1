6e8acc300afa06e3882ede6f3f51aca5fefbeb29
Mark Shevelev <lionishy@gmail.com>
2020-05-29 17:10:03 +0300

    discretization changed to utilize device tables

--- WHFI-CUDA-1.1/diagonal_discretization.cuh
+++ WHFI-CUDA-1.1/diagonal_discretization.cuh
@@ -1,11 +1,19 @@
 #pragma once
 
+#include "DeviceTable.cuh"
+
 #include <cuda_runtime.h>
 
 namespace iki { namespace diffusion { namespace device {
 	template <typename T>
 	inline
-	__device__ T diagonal_discretization(T x_im1_j, T x_i_j, T x_ip1_j, T d_im1_j, T d_i_j) {
-		return d_i_j * (x_ip1_j - x_i_j) - d_im1_j * (x_i_j - x_im1_j);
+		__device__ T along_diagonal_discretization(table::device::DeviceTable<T> x_curr, table::device::DeviceTable<T> along_dfc, unsigned row_idx, unsigned elm_idx) {
+		return along_dfc(row_idx, elm_idx) * (x_curr(row_idx, elm_idx + 1) - x_curr(row_idx, elm_idx)) - along_dfc(row_idx, elm_idx - 1) * (x_curr(row_idx, elm_idx) - x_curr(row_idx, elm_idx - 1));
+	}
+
+	template <typename T>
+	inline
+		__device__ T perp_diagonal_discretization(table::device::DeviceTable<T> x_curr, table::device::DeviceTable<T> perp_dfc, unsigned row_idx, unsigned elm_idx) {
+		return perp_dfc(elm_idx,row_idx) * (x_curr(row_idx+1, elm_idx) - x_curr(row_idx, elm_idx)) - perp_dfc(elm_idx, row_idx - 1) * (x_curr(row_idx, elm_idx) - x_curr(row_idx-1, elm_idx));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
