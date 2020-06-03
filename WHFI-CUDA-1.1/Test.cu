116a93139ce4d34855fa7062963ca8a7d7c58be8
Mark Shevelev <lionishy@gmail.com>
2020-06-03 21:49:23 +0300

    resonant velocity solver test with data lines

--- WHFI-CUDA-1.1/Test.cu
+++ WHFI-CUDA-1.1/Test.cu
@@ -121,22 +121,29 @@ void perp_mixed_dfc_field_int(iki::grid::HostGrid<float> &dfc_grid) {
 #include <stdexcept>
 
 int main() {
-	iki::whfi::PhysicalParamenters<float> params = iki::whfi::init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -1.0f);
+	iki::whfi::PhysicalParamenters<float> params = iki::whfi::init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.0f);
 	auto wk_solver = iki::whfi::ResonantVelocitySolver<float>(iki::whfi::make_ZFunc(1.e-4f,10.f), params);
 
-	iki::grid::Axis<float> vparall_axis = { -15.0f, 1.e-2 };
+	unsigned vparall_size = 1024;
+	iki::grid::Axis<float> vparall_axis = { -16.5f, 1.5e-2 };
+	iki::grid::HostGridLine<float> w_line(vparall_axis, vparall_size);
+	iki::grid::HostGridLine<float> k_line(vparall_axis, vparall_size);
 
 	try {
+		auto solver = iki::whfi::ResonantVelocitySolver<float>(iki::whfi::make_ZFunc(1.e-5f, 15.f), params);
+		for (unsigned idx = 0; idx != vparall_size; ++idx) {
+			auto wk_pair = solver(vparall_axis(idx));
+			w_line.line(idx) = wk_pair.first;
+			k_line.line(idx) = wk_pair.second;
+		}
+			
 		std::ofstream ascii_out;
 		ascii_out.exceptions(std::ios::badbit | std::ios::failbit);
 
 		ascii_out.open("./data/vres-omega-k-test.txt");
 		ascii_out.precision(7); ascii_out.setf(std::ios::fixed, std::ios::floatfield);
-		for (unsigned idx = 0; true; ++idx) {
-			auto v_res = vparall_axis(idx);
-			if (v_res > -0.9f) break;
-			auto wk_pair = wk_solver(v_res);
-			ascii_out << v_res << ' ' << wk_pair.first << ' ' << wk_pair.second << std::endl;
+		for (unsigned idx = 0; idx != vparall_size; ++idx) {
+			ascii_out << vparall_axis(idx) << ' ' << w_line.line(idx) << ' ' << k_line.line(idx) << '\n';
 		}
 	}
 	catch (std::exception const &ex) {
