670eff29675df2fbb41dded434c980fdb7761806
Mark Shevelev <lionishy@gmail.com>
2020-06-06 09:07:01 +0300

    integral approximation improved with the 3 over 8 formula

--- WHFI-CUDA-1.1/first_moment_kernel.cuh
+++ WHFI-CUDA-1.1/first_moment_kernel.cuh
@@ -11,10 +11,10 @@ namespace iki { namespace whfi { namespace device {
 	__global__ void first_moment_kernel(table::device::DeviceTable<T> const vdf_table, T const begin, T const step, table::device::DeviceDataLine<T> zero_moment) {
 		unsigned row_idx = threadIdx.x + blockDim.x * blockIdx.x;
  
-		T sum = T(0.);
-		for (unsigned elm_idx = 2; elm_idx != vdf_table.row_size; elm_idx += 2) {
-			auto arg_m = begin + step * (elm_idx - 1), arg = begin + step * elm_idx, arg_p = begin + step * (elm_idx + 1);
-			sum += step / T(3.) * (arg_m * vdf_table(row_idx, elm_idx - 1) + T(4.) * arg * vdf_table(row_idx, elm_idx) + arg_p * vdf_table(row_idx, elm_idx + 1));
+		T sum = T(0.); //3/8 formula
+		for (unsigned elm_idx = 0; elm_idx + 3 < vdf_table.row_size; elm_idx += 3) {
+			auto arg1 = begin + step * elm_idx, arg2 = arg1 + step, arg3 = arg2 + step, arg4 = arg3 + step;
+			sum += T(3. / 8.) * step * (arg1 * vdf_table(row_idx, elm_idx) + T(3.) * arg2 * vdf_table(row_idx, elm_idx + 1) + T(3.) * arg3 * vdf_table(row_idx, elm_idx + 2) + arg4 * vdf_table(row_idx, elm_idx + 3));
 		}
 		zero_moment(row_idx) = sum;
 	}
--- WHFI-CUDA-1.1/zero_moment_kernel.cuh
+++ WHFI-CUDA-1.1/zero_moment_kernel.cuh
@@ -11,9 +11,9 @@ namespace iki { namespace whfi { namespace device {
 	__global__ void zero_moment_kernel(table::device::DeviceTable<T> const vdf_table, T const begin, T const step, table::device::DeviceDataLine<T> zero_moment) {
 		unsigned row_idx = threadIdx.x + blockDim.x * blockIdx.x;
  
-		T sum = T(0.);
-		for (unsigned elm_idx = 2; elm_idx != vdf_table.row_size; elm_idx += 2) {
-			sum += step / T(3.) * (vdf_table(row_idx, elm_idx - 1) + T(4.) * vdf_table(row_idx, elm_idx) + vdf_table(row_idx, elm_idx + 1));
+		T sum = T(0.); //3/8 formula
+		for (unsigned elm_idx = 0; elm_idx + 3 < vdf_table.row_size; elm_idx += 3) {
+			sum += T(3. / 8.) * step * (vdf_table(row_idx, elm_idx) + T(3.) * vdf_table(row_idx, elm_idx + 1) + T(3.) * vdf_table(row_idx, elm_idx + 2) + vdf_table(row_idx, elm_idx + 3));
 		}
 		zero_moment(row_idx) = sum;
 	}
