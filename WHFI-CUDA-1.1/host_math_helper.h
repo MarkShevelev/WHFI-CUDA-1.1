d273c760ec415d154ba8d6b9229fb44e0d759f2c
Mark Shevelev <lionishy@gmail.com>
2020-06-06 16:47:24 +0300

    template made inline

--- WHFI-CUDA-1.1/host_math_helper.h
+++ WHFI-CUDA-1.1/host_math_helper.h
@@ -2,21 +2,25 @@
 
 namespace iki { namespace math { 
 	template <unsigned exp>
+	inline
 	float pow(float base) {
 		return base * pow<exp - 1>(base);
 	}
 
 	template <>
+	inline
 	float pow<1>(float base) {
 		return base;
 	}
 
 	template <unsigned exp>
+	inline
 	double pow(double base) {
 		return base * pow<exp - 1>(base);
 	}
 
 	template <>
+	inline
 	double pow<1>(double base) {
 		return base;
 	}
