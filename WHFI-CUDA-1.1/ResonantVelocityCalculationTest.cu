3f26540e884041d88f758f475d1297adc19fa2bb
Mark Shevelev <lionishy@gmail.com>
2020-06-06 09:43:49 +0300

    resonant velocity calculation test refactored into separate function

--- WHFI-CUDA-1.1/ResonantVelocityCalculationTest.cu
+++ WHFI-CUDA-1.1/ResonantVelocityCalculationTest.cu
@@ -4,37 +4,39 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <stdexcept>
 
-int float_fesonant_velocity_calculation_test() {
-	iki::whfi::PhysicalParameters<float> params = iki::whfi::init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.0f);
-	auto wk_solver = iki::whfi::ResonantVelocitySolver<float>(iki::whfi::make_ZFunc(1.e-4f, 10.f), params);
+using namespace std;
+using namespace iki;
+using namespace iki::whfi;
+using namespace iki::grid;
+using namespace iki::table;
 
-	unsigned vparall_size = 1024;
-	iki::grid::Axis<float> vparall_axis = { -16.5f, 1.5e-2f };
+void float_resonant_velocity_calculation_test(PhysicalParameters<float> params, Axis<float> vparall_axis, Axis<float> vperp_axis, unsigned vparall_size, unsigned vperp_size) {
+	auto wk_solver = iki::whfi::ResonantVelocitySolver<float>(iki::whfi::make_ZFunc(1.e-5f, 15.f), params);
+
 	iki::grid::HostGridLine<float> w_line(vparall_axis, vparall_size);
 	iki::grid::HostGridLine<float> k_line(vparall_axis, vparall_size);
 
-	try {
-		auto solver = iki::whfi::ResonantVelocitySolver<float>(iki::whfi::make_ZFunc(1.e-5f, 15.f), params);
-		for (unsigned idx = 0; idx != vparall_size; ++idx) {
-			auto wk_pair = solver(vparall_axis(idx));
-			w_line.line(idx) = wk_pair.first;
-			k_line.line(idx) = wk_pair.second;
-		}
-
-		std::ofstream ascii_out;
-		ascii_out.exceptions(std::ios::badbit | std::ios::failbit);
-
-		ascii_out.open("./data/vres-omega-k-test.txt");
-		ascii_out.precision(7); ascii_out.setf(std::ios::fixed, std::ios::floatfield);
-		for (unsigned idx = 0; idx != vparall_size; ++idx) {
-			ascii_out << vparall_axis(idx) << ' ' << w_line.line(idx) << ' ' << k_line.line(idx) << '\n';
-		}
-	}
-	catch (std::exception const &ex) {
-		std::cout << ex.what() << std::endl;
+	for (unsigned idx = 0; idx != vparall_size; ++idx) {
+		auto wk_pair = wk_solver(vparall_axis(idx));
+		w_line.line(idx) = wk_pair.first;
+		k_line.line(idx) = wk_pair.second;
 	}
 
-	return 0;
+	std::ofstream ascii_out;
+	ascii_out.exceptions(std::ios::badbit | std::ios::failbit);
+
+	{
+		stringstream filename_ss;
+		filename_ss.precision(2); filename_ss.setf(ios::fixed, ios::floatfield);
+		filename_ss << "./data/vres-w-k-" << params.betta_c << '-' << params.bulk_to_alfven_c << "-test.txt";
+		ascii_out.open(filename_ss.str());
+	}
+	
+	ascii_out.precision(7); ascii_out.setf(std::ios::scientific, std::ios::floatfield);
+	for (unsigned idx = 0; idx != vparall_size; ++idx) {
+		ascii_out << vparall_axis(idx) << ' ' << w_line.line(idx) << ' ' << k_line.line(idx) << '\n';
+	}
 }
