32c97a55e39a6a734ffe92f097af1430b3e7dacd
Mark Shevelev <lionishy@gmail.com>
2020-06-15 21:14:36 +0300

    the main funcion used the new template function to launch vdf evolution

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -1,5 +1,6 @@
 #include "PhysicalParameters.h"
 #include "HostGrid.h"
+#include "template_vdf_diffusion.cuh"
 
 #include <iostream>
 #include <exception>
@@ -10,16 +11,20 @@ using namespace iki::whfi;
 using namespace iki::table;
 using namespace iki::grid;
 
-void float_diffusion_coefficient_test(PhysicalParameters<float> params, Axis<float> vparall_axis, Axis<float> vperp_axis, unsigned vparall_size, unsigned vperp_size);
-
-void float_vdf_diffusion_test(PhysicalParameters<float> params, Axis<float> vparall_axis, Axis<float> vperp_axis, unsigned vparall_size, unsigned vperp_size, float dt, unsigned iter);
-
 int main() {
 	try {
 		auto params = init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.f);
 		Axis<float> vparall_axis = { -16.3f, 3e-2f }; unsigned vparall_size = 512;
 		Axis<float> vperp_axis = { -3e-2f, 3e-2f }; unsigned vperp_size = 1024;
-		float_vdf_diffusion_test(params, vparall_axis, vperp_axis, vparall_size, vperp_size, 1.e-2f, 1000);
+		vdf_diffusion<float>(
+			params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
+			1.0e-5f, 0.f, //amplitude, amplitude time
+			10000, 0.1f,        //iterations, dt
+			true,
+			true,
+			true,
+			true,
+			true);
 	} 
 	catch(std::exception const &ex) {
 		cout << ex.what() << endl;
