7a4e35d3c74e79e8bfdbc67234b460e22dd46771
Mark Shevelev <lionishy@gmail.com>
2020-06-19 10:37:11 +0300

    minimal amplitude added

--- WHFI-CUDA-1.1/amplitude_recalculation_kernel.cuh
+++ WHFI-CUDA-1.1/amplitude_recalculation_kernel.cuh
@@ -7,9 +7,10 @@
 
 namespace iki { namespace whfi { namespace device { 
 	template <typename T>
-	__global__ void amplitude_recalculation_kernel(table::device::DeviceDataLine<T> const growth_rate, table::device::DeviceDataLine<T> amplitude_spectrum, T dt) {
+	__global__ void amplitude_recalculation_kernel(table::device::DeviceDataLine<T> const growth_rate, table::device::DeviceDataLine<T> amplitude_spectrum, T dt, T noise_amplitude) {
 		unsigned vparall_idx = threadIdx.x + blockIdx.x * blockDim.x;
-
+		
 		amplitude_spectrum(vparall_idx) *= 1.0 + 2 * dt * growth_rate(vparall_idx);
+		if (amplitude_spectrum(vparall_idx) < noise_amplitude) amplitude_spectrum(vparall_idx) = noise_amplitude;
 	}
 }/*device*/ }/*whfi*/ }/*iki*/
