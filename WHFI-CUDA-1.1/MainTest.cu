b8c5c8e7a7605394c922930d7f40b92ec20a27ae
Mark Shevelev <lionishy@gmail.com>
2020-06-19 10:40:34 +0300

    main test rewritten

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -13,14 +13,14 @@ using namespace iki::grid;
 
 int main() {
 	try {
-		if (false) {
+		if (true) {
 			auto params = init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.f);
 			Axis<float> vparall_axis = { -16.3f, 3e-2f }; unsigned vparall_size = 512;
 			Axis<float> vperp_axis = { -3e-2f, 3e-2f }; unsigned vperp_size = 1024;
 			vdf_diffusion<float>(
 				params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
 				1.0e-5f, 0.f, //amplitude, amplitude time
-				10000, 0.1f,        //iterations, dt
+				40000, 0.1f,  //iterations, dt
 				true,
 				true,
 				true,
@@ -28,14 +28,14 @@ int main() {
 				true);
 		}
 
-		if (true) {
+		if (false) {
 			auto params = init_parameters(0.85, 1.0 / 0.85, 0.25, -9.);
 			Axis<double> vparall_axis = { -16.3, 3e-2 }; unsigned vparall_size = 512;
 			Axis<double> vperp_axis = { -3e-2, 3e-2 }; unsigned vperp_size = 1024;
 			vdf_diffusion<double>(
 				params, Space<double>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
 				1.0e-5, 0., //amplitude, amplitude time
-				10000, 0.1,        //iterations, dt
+				4000, 1.0,        //iterations, dt
 				true,
 				true,
 				true,
