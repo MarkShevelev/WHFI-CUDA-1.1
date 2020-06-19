941d110ab063ebff8ac6c6ba35bd1c211adf4a96
Mark Shevelev <lionishy@gmail.com>
2020-06-19 11:23:44 +0300

    moments and moments table splitted

--- WHFI-CUDA-1.1/AnalyticalMoments.h
+++ WHFI-CUDA-1.1/AnalyticalMoments.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PhysicalParameters.h"
+#include "HostGrid.h"
 #include "host_math_helper.h"
 
 #include <vector>
@@ -12,27 +13,49 @@ namespace iki { namespace whfi {
 	public:
 		AnalyticalMoments(PhysicalParameters<T> p) : p(p) { }
 
-		std::vector<T> g(T vparall_begin, T vparall_step, unsigned size) const {
-			auto g_vparall = [this] (T vparall) {
-				return p.nc * std::exp(-math::pow<2>(vparall - p.bulk_to_term_c) / T(2)) + p.nh * std::sqrt(p.TcTh_ratio) * exp(-math::pow<2>(vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) / T(2));
-			};
-			std::vector<T> table(size);
-			for (unsigned count = 0u; count < size; ++count)
-				table[count] = g_vparall(vparall_begin + vparall_step * count);
-			return table;
+		T g(T vparall) const {
+			return p.nc * std::exp(-math::pow<2>(vparall - p.bulk_to_term_c) / T(2)) + p.nh * std::sqrt(p.TcTh_ratio) * exp(-math::pow<2>(vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) / T(2));
 		}
 
-		std::vector<T> G(T vparall_begin, T vparall_step, unsigned size) const {
-			auto G_vparall = [this] (T vparall) {
-				return p.nc * std::exp(-math::pow<2>(vparall - p.bulk_to_term_c) / T(2)) + p.nh * std::sqrt(T(1) / p.TcTh_ratio) * exp(-math::pow<2>(vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) / T(2));
-			};
-			std::vector<T> table(size);
-			for (unsigned count = 0u; count < size; ++count)
-				table[count] = G_vparall(vparall_begin + vparall_step * count);
-			return table;
+		T G(T vparall) const {
+			return p.nc * std::exp(-math::pow<2>(vparall - p.bulk_to_term_c) / T(2)) + p.nh * std::sqrt(T(1) / p.TcTh_ratio) * exp(-math::pow<2>(vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) / T(2));
+		}
+
+		T GDerive(T vparall) const {
+			return -(vparall - p.bulk_to_alfven_c) * p.nc * std::exp(-math::pow<2>(vparall - p.bulk_to_term_c) / T(2)) - (vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) * p.nh * exp(-math::pow<2>(vparall * std::sqrt(p.TcTh_ratio) - p.bulk_to_term_h) / T(2));
 		}
 
 	private:
 		iki::whfi::PhysicalParameters<T> p;
+	};
+
+	template <typename T>
+	class AnalyticalMomentsTable final {
+	public:
+		AnalyticalMomentsTable(PhysicalParameters<T> p) : moments(p) { }
+
+		std::vector<T> g(grid::Axis<T> vparall_axis, unsigned size) const {
+			std::vector<T> table(size);
+			for (unsigned idx = 0u; idx < size; ++idx)
+				table[idx] = moments.g(vparall_axis(idx));
+			return table;
+		}
+
+		std::vector<T> G(grid::Axis<T> vparall_axis, unsigned size) const {
+			std::vector<T> table(size);
+			for (unsigned idx = 0u; idx < size; ++idx)
+				table[idx] = moments.G(vparall_axis(idx));
+			return table;
+		}
+
+		std::vector<T> GDerive(grid::Axis<T> vparall_axis, unsigned size) const {
+			std::vector<T> table(size);
+			for (unsigned idx = 0u; idx < size; ++idx)
+				table[idx] = moments.GDerive(vparall_axis(idx));
+			return table;
+		}
+
+	private:
+		AnalyticalMoments<T> moments;
 	};
 } /* whfi */ } /* iki */
