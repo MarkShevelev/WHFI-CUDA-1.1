92229b3e98fb702de3b1559228a0f41a9896165a
Mark Shevelev <lionishy@gmail.com>
2020-06-20 08:10:24 +0300

    diffusion test with automatic vparall_axis calculation from the growth rate

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -1,6 +1,8 @@
 #include "PhysicalParameters.h"
 #include "HostGrid.h"
 #include "template_vdf_diffusion.cuh"
+#include "ZFunc.h"
+#include "construct_vparall_axis.h"
 
 #include <iostream>
 #include <exception>
@@ -11,16 +13,19 @@ using namespace iki::whfi;
 using namespace iki::table;
 using namespace iki::grid;
 
+void float_automated_vparall_axis_test(PhysicalParameters<float> params, unsigned vparall_size, float begin, float end);
+
 int main() {
 	try {
 		if (true) {
 			auto params = init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.f);
-			Axis<float> vparall_axis = { -16.3f, 3e-2f }; unsigned vparall_size = 512;
-			Axis<float> vperp_axis = { -3e-2f, 3e-2f }; unsigned vperp_size = 1024;
+			unsigned vparall_size = 512; unsigned vperp_size = 1024;
+			Axis<float> vparall_axis = construct_vparall_axis<float>(params,make_ZFunc<float>(1.e-5f, 15.f), vparall_size, -7.f, -0.96f);
+			Axis<float> vperp_axis = { -3e-2f, 3e-2f }; 
 			vdf_diffusion<float>(
 				params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
-				1.0e-5f, 0.f, //amplitude, amplitude time
-				40000, 0.1f,  //iterations, dt
+				1.0e-6f, 0.f, //amplitude, amplitude time
+				20000, 0.1f,  //iterations, dt
 				true,
 				true,
 				true,
@@ -35,7 +40,7 @@ int main() {
 			vdf_diffusion<double>(
 				params, Space<double>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
 				1.0e-5, 0., //amplitude, amplitude time
-				4000, 1.0,        //iterations, dt
+				10000, 0.1,        //iterations, dt
 				true,
 				true,
 				true,
