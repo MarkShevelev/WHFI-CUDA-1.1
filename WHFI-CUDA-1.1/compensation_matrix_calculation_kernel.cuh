ccfd1ba42b00845cfb2c98ecc477791c84152a0d
Mark Shevelev <lionishy@gmail.com>
2020-06-21 18:19:12 +0300

    changed to central_mixed_discretization

--- WHFI-CUDA-1.1/compensation_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/compensation_matrix_calculation_kernel.cuh
@@ -11,17 +11,13 @@ namespace iki { namespace diffusion { namespace device {
 	__global__ void  compensation_matrix_calculation_kernel(
 		table::device::DeviceTable<T> d,
 		table::device::DeviceTable<T> const x_prev, table::device::DeviceTable<T> const x_next,
-		table::device::DeviceTable<T> const along_mixed_dfc, table::device::DeviceTable<T> const perp_mixed_dfc, T mixed_r
+		table::device::DeviceTable<T> const along_mixed_dfc, T mixed_r
 	) {
 		unsigned row_idx = blockIdx.x * TILE_SIZE + threadIdx.x;
 		unsigned elm_idx = blockIdx.y * TILE_SIZE + threadIdx.y;
 
 		if (0 == row_idx || x_prev.row_count - 1 == row_idx || 0 == elm_idx || x_prev.row_size - 1 == elm_idx) return;
 
-		d(row_idx, elm_idx) +=
-			0.5 * mixed_r * along_mixed_term_discretization(x_next, along_mixed_dfc, row_idx, elm_idx)
-			+ 0.5 * mixed_r * perp_mixed_term_discretization(x_next, perp_mixed_dfc, row_idx, elm_idx)
-			- 0.5 * mixed_r * along_mixed_term_discretization(x_prev, along_mixed_dfc, row_idx, elm_idx)
-			- 0.5 * mixed_r * perp_mixed_term_discretization(x_prev, perp_mixed_dfc, row_idx, elm_idx);
+		d(row_idx, elm_idx) += 0.5 * mixed_r * (central_mixed_term_discretization(x_next,along_mixed_dfc,row_idx,elm_idx) - central_mixed_term_discretization(x_prev, along_mixed_dfc, row_idx, elm_idx));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
