888d7d48e962c33e29ddc701dc81b8dc59f46b25
Mark Shevelev <lionishy@gmail.com>
2020-06-21 16:53:50 +0300

    zero gradient conditions added

--- WHFI-CUDA-1.1/neuman_boundary_condition.cuh
+++ WHFI-CUDA-1.1/neuman_boundary_condition.cuh
@@ -18,4 +18,15 @@ namespace iki { namespace diffusion { namespace device {
 		vdf(vdf.row_count-1,elm_idx) = vdf(vdf.row_count-2, elm_idx);
 	}
 
+	template <typename T>
+	__global__ void along_axis_min_boundary_kernel(table::device::DeviceTable<T> vdf) {
+		unsigned row_idx = threadIdx.x + blockDim.x * blockIdx.x;
+		vdf(row_idx, 0) = vdf(row_idx, 1);
+	}
+
+	template <typename T>
+	__global__ void perp_axis_min_boundary_kernel(table::device::DeviceTable<T> vdf) {
+		unsigned elm_idx = threadIdx.x + blockDim.x * blockIdx.x;
+		vdf(0, elm_idx) = vdf(1, elm_idx);
+	}
 }/*device*/ }/*diffusion*/ }/*iki*/
