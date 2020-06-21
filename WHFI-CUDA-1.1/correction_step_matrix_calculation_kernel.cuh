910d77be3630e80d569d2403e9d66a78d6884252
Mark Shevelev <lionishy@gmail.com>
2020-06-21 18:18:52 +0300

    changed to advection

--- WHFI-CUDA-1.1/correction_step_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/correction_step_matrix_calculation_kernel.cuh
@@ -11,16 +11,19 @@ namespace iki { namespace diffusion { namespace device {
 	__global__ void  correction_step_matrix_calculation_kernel(
 		table::device::DeviceTable<T> a, table::device::DeviceTable<T> b, table::device::DeviceTable<T> c, table::device::DeviceTable<T> d,
 		table::device::DeviceTable<T> const x_prev, table::device::DeviceTable<T> const x_next,
-		table::device::DeviceTable<T> const along_dfc, T along_r) {
+		table::device::DeviceTable<T> const along_dfc, T along_r, 
+		table::device::DeviceDataLine<T> const perp_mixed_dfc, T mixed_r
+	) {
 		unsigned row_idx = blockIdx.x * TILE_SIZE + threadIdx.x;
 		unsigned elm_idx = blockIdx.y * TILE_SIZE + threadIdx.y;
 
 		if (0 == row_idx || x_prev.row_count - 1 == row_idx || 0 == elm_idx || x_prev.row_size - 1 == elm_idx) return;
 
-		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx - 1);
+		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx - 1) + 0.5 * 0.25 * mixed_r * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx));
 		b(row_idx, elm_idx) = T(1) + 0.5 * along_r * (along_dfc(row_idx, elm_idx-1) + along_dfc(row_idx, elm_idx));
-		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx);
+		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx) - 0.5 * 0.25 * mixed_r * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx));
 		d(row_idx, elm_idx) = x_next(row_idx, elm_idx)
-			- 0.5 * along_r * along_diagonal_discretization(x_prev,along_dfc,row_idx,elm_idx);
+			- 0.5 * along_r * along_diagonal_discretization(x_prev,along_dfc,row_idx,elm_idx)
+			- 0.5 * mixed_r * ((perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx)) * (x_prev(row_idx,elm_idx+1) - x_prev(row_idx,elm_idx-1));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
