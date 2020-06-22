e725504da0ed6cf76dd919c6e7253b582cc4b7e7
Mark Shevelev <lionishy@gmail.com>
2020-06-22 12:51:53 +0300

    advection coefficient added

--- WHFI-CUDA-1.1/correction_step_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/correction_step_matrix_calculation_kernel.cuh
@@ -19,11 +19,12 @@ namespace iki { namespace diffusion { namespace device {
 
 		if (0 == row_idx || x_prev.row_count - 1 == row_idx || 0 == elm_idx || x_prev.row_size - 1 == elm_idx) return;
 
-		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx - 1) + 0.5 * 0.25 * mixed_r * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx));
+		T adv_v = 0.25 * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx));
+		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx - 1) + 0.5 *  mixed_r * adv_v;
 		b(row_idx, elm_idx) = T(1) + 0.5 * along_r * (along_dfc(row_idx, elm_idx-1) + along_dfc(row_idx, elm_idx));
-		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx) - 0.5 * 0.25 * mixed_r * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx));
+		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx) - 0.5 * mixed_r * adv_v;
 		d(row_idx, elm_idx) = x_next(row_idx, elm_idx)
 			- 0.5 * along_r * along_diagonal_discretization(x_prev,along_dfc,row_idx,elm_idx)
-			- 0.5 * mixed_r * (perp_mixed_dfc(row_idx + 1, elm_idx) - perp_mixed_dfc(row_idx - 1, elm_idx)) * (x_prev(row_idx,elm_idx+1) - x_prev(row_idx,elm_idx-1));
+			- 0.5 * mixed_r * adv_v * (x_prev(row_idx,elm_idx+1) - x_prev(row_idx,elm_idx-1));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
