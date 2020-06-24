6ccaba4a308c1db19b74238cd3394b827d6a09e9
Mark Shevelev <lionishy@gmail.com>
2020-06-24 13:54:24 +0300

    main test

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -13,16 +13,13 @@ using namespace iki::whfi;
 using namespace iki::table;
 using namespace iki::grid;
 
-void fdiffusion_test();
-
 int main() {
 	try {
-		fdiffusion_test();
-		/*if (false) {
+		if (true) {
 			auto params = init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.f);
 			unsigned vparall_size = 512; unsigned vperp_size = 2 * 2048;
 			Axis<float> vparall_axis = construct_vparall_axis<float>(params,make_ZFunc<float>(1.e-5f, 15.f), vparall_size, -9.f, -0.96f);
-			Axis<float> vperp_axis = { -1.5e-2f, 1.5e-2f }; 
+			Axis<float> vperp_axis = { -0.75e-2f, 1.5e-2f }; 
 			vdf_diffusion<float>(
 				params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
 				1.0e-5f, 0.f, //amplitude, amplitude time
@@ -32,7 +29,7 @@ int main() {
 				true,
 				true,
 				true);
-		}
+		} 
 
 		if (false) {
 			auto params = init_parameters(0.85, 1.0 / 0.85, 0.25, -9.);
@@ -47,7 +44,7 @@ int main() {
 				true,
 				true,
 				true);
-		}*/
+		}
 	} 
 	catch(std::exception const &ex) {
 		cout << ex.what() << endl;
