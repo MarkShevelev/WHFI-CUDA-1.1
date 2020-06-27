c05727e857fe3cb00a25bbbcdf525623acbe83c7
Mark Shevelev <lionishy@gmail.com>
2020-06-27 14:28:44 +0300

    growth rate at the boundary calculated with a one directional derivative approach

--- WHFI-CUDA-1.1/growth_rate_kernel.cuh
+++ WHFI-CUDA-1.1/growth_rate_kernel.cuh
@@ -16,9 +16,15 @@ namespace iki { namespace whfi { namespace device {
 		iki::table::device::DeviceDataLine<T> gamma
 	) {
 		unsigned idx = threadIdx.x + blockDim.x * blockIdx.x;
-		if (0 == idx || first_moment.size - 1 == idx) { gamma(idx) = T(0.);  return; }
-
-		auto first_moment_derive = T(0.5) * (first_moment(idx + 1) - first_moment(idx - 1)) / vparall_step;
+		T first_moment_derive = T(0);
+		if (0 == idx) {
+			first_moment_derive = T(0.5) / vparall_step * (-first_moment(idx + 2) + 4 * first_moment(idx + 1) - 3 * first_moment(idx));
+		} else if (first_moment.size - 1 == idx) {
+			first_moment_derive = T(0.5) / vparall_step * (first_moment(idx -2) - 4 * first_moment(idx-1) + 3 * first_moment(idx));
+		}
+		else {
+			first_moment_derive = T(0.5) * (first_moment(idx + 1) - first_moment(idx - 1)) / vparall_step;
+		}
 
 		//\sqrt(\frac{\pi}{2}) = 1.25331414
 		gamma(idx) = -T(1.25331414) * (first_moment_derive - zero_moment(idx) / k_betta(idx)) / dispersion_derive(idx);
