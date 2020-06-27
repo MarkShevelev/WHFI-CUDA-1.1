a9c9d873c44de6bcddcd57b3d08b3f9ac8063194
Mark Shevelev <lionishy@gmail.com>
2020-06-27 15:23:03 +0300

    nc 0.95 bettac 1.0 ThTc 6. UcVa -2. -- refactored according to the new diffusion function signature

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -18,34 +18,35 @@ void float_automated_vparall_axis_test(PhysicalParameters<float> params, unsigne
 int main() {
 	try {
 		if (true) {
-			auto params = init_parameters(0.85f, 1.0f / 0.85f, 0.25f, -9.f);
+			auto params = init_parameters(0.95f, 1.0f / 0.95f, 1.f/6.f, -2.f);
 			unsigned vparall_size = 512; unsigned vperp_size = 2 * 2048;
 			Axis<float> vparall_axis = construct_vparall_axis<float>(params,make_ZFunc<float>(1.e-5f, 15.f), vparall_size, -9.f, -0.96f);
-			Axis<float> vperp_axis = { -0.75e-2f, 1.5e-2f }; 
+			Axis<float> vperp_axis = { -1e-2f, 2e-2f }; 
 			vdf_diffusion<float>(
 				params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
-				1.0e-5f, 0.f, //amplitude, amplitude time
-				10000, 0.1f,  //iterations, dt
-				true,
-				true,
-				true,
-				true,
-				true);
+				1.0e-10f, 0.f, //amplitude, amplitude time
+				10000, 1.f,  //iterations, dt
+				false,         //initial core dfc 
+				true, 1000,    //intermidiate growth rate
+				false, 1000,   //intermidiate amplitude
+				false, 1000    //intermidiate vdf
+			);
 		}
 
 		if (false) {
-			auto params = init_parameters(0.85, 1.0 / 0.85, 0.25, -9.);
-			Axis<double> vparall_axis = { -16.3, 3e-2 }; unsigned vparall_size = 512;
-			Axis<double> vperp_axis = { -3e-2, 3e-2 }; unsigned vperp_size = 1024;
+			auto params = init_parameters(0.95, 1.0 / 0.95, 1./6., -2.);
+			unsigned vparall_size = 512; unsigned vperp_size = 2 * 2048;
+			Axis<double> vparall_axis = construct_vparall_axis<double>(params, make_ZFunc<double>(1.e-5, 15.), vparall_size, -9., -0.96);
+			Axis<double> vperp_axis = { -1e-2, 2e-2 };
 			vdf_diffusion<double>(
 				params, Space<double>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
-				1.0e-5, 0., //amplitude, amplitude time
-				10000, 0.1,        //iterations, dt
-				true,
-				true,
-				true,
-				true,
-				true);
+				1.0e-10, 0., //amplitude, amplitude time
+				10000, 1.f,  //iterations, dt
+				false,         //initial core dfc 
+				true, 1000,    //intermidiate growth rate
+				false, 1000,   //intermidiate amplitude
+				false, 1000    //intermidiate vdf
+			);
 		}
 	} 
 	catch(std::exception const &ex) {
