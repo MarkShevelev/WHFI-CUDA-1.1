c322b676eedbfd429b73fe1da52a184586e0acab
Mark Shevelev <mposimba@gmail.com>
2020-06-30 17:44:39 +0300

    boundary values are duplicated from the nearest neighbor

--- WHFI-CUDA-1.1/growth_rate_kernel.cuh
+++ WHFI-CUDA-1.1/growth_rate_kernel.cuh
@@ -16,17 +16,14 @@ namespace iki { namespace whfi { namespace device {
 		iki::table::device::DeviceDataLine<T> gamma
 	) {
 		unsigned idx = threadIdx.x + blockDim.x * blockIdx.x;
-		T first_moment_derive = T(0);
-		if (0 == idx) {
-			first_moment_derive = T(0.5) / vparall_step * (-first_moment(idx + 2) + 4 * first_moment(idx + 1) - 3 * first_moment(idx));
-		} else if (first_moment.size - 1 == idx) {
-			first_moment_derive = T(0.5) / vparall_step * (first_moment(idx -2) - 4 * first_moment(idx-1) + 3 * first_moment(idx));
-		}
-		else {
-			first_moment_derive = T(0.5) * (first_moment(idx + 1) - first_moment(idx - 1)) / vparall_step;
-		}
+		if (0 == idx || first_moment.size - 1 == idx) return;
+		
+		auto first_moment_derive = T(0.5) * (first_moment(idx + 1) - first_moment(idx - 1)) / vparall_step;
 
 		//\sqrt(\frac{\pi}{2}) = 1.25331414
 		gamma(idx) = -T(1.25331414) * (first_moment_derive - zero_moment(idx) / k_betta(idx)) / dispersion_derive(idx);
+
+		if (1 == idx) { gamma(0) == gamma(idx); }
+		if (first_moment.size - 2 == idx) { gamma(first_moment.size - 1) = gamma(idx); }
 	}
 }/*device*/ }/*whfi*/ }/*iki*/
