20bafb2a5c914b6a39552e4e5c2999038c9890bc
Mark Shevelev <mposimba@gmail.com>
2020-06-30 15:32:30 +0300

    minor formatting changes

--- WHFI-CUDA-1.1/mixed_term_discretization.cuh
+++ WHFI-CUDA-1.1/mixed_term_discretization.cuh
@@ -11,10 +11,10 @@ namespace iki { namespace diffusion { namespace device {
 		T central_grad = x_curr(row_idx + 1, elm_idx) - x_curr(row_idx - 1, elm_idx);
 		T left_grad = x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx - 1);
 
-		T right_q = along_mixed_dfc(row_idx, elm_idx) * 0.5 * (right_grad + central_grad);
-		T left_q = along_mixed_dfc(row_idx, elm_idx-1) * 0.5 * (left_grad + central_grad);
+		T right_q = along_mixed_dfc(row_idx, elm_idx) * (right_grad + central_grad);
+		T left_q = along_mixed_dfc(row_idx, elm_idx-1) * (left_grad + central_grad);
 
-		return 0.25 * (right_q - left_q);
+		return T(0.25) * (right_q - left_q);
 	}
 
 	template <typename T>
@@ -23,14 +23,14 @@ namespace iki { namespace diffusion { namespace device {
 		T central_grad = x_curr(row_idx, elm_idx + 1) - x_curr(row_idx, elm_idx - 1);
 		T left_grad = x_curr(row_idx - 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx - 1);
 
-		T right_q = perp_mixed_dfc(elm_idx, row_idx) * 0.5 * (right_grad + central_grad);
-		T left_q = perp_mixed_dfc(elm_idx, row_idx - 1) * 0.5 * (left_grad + central_grad);
+		T right_q = perp_mixed_dfc(elm_idx, row_idx) * (right_grad + central_grad);
+		T left_q = perp_mixed_dfc(elm_idx, row_idx - 1) * (left_grad + central_grad);
 
-		return 0.25 * (right_q - left_q);
+		return T(0.25) * (right_q - left_q);
 	}
 
 	template <typename T>
 	__device__ T central_mixed_term_discretization(table::device::DeviceTable<T> x_curr, table::device::DeviceTable<T> mixed_dfc, unsigned row_idx, unsigned elm_idx) {
-		return 0.5 * mixed_dfc(row_idx, elm_idx) * (x_curr(row_idx + 1, elm_idx + 1) + x_curr(row_idx - 1, elm_idx - 1) - x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx + 1));
+		return T(0.5) * mixed_dfc(row_idx, elm_idx) * (x_curr(row_idx + 1, elm_idx + 1) + x_curr(row_idx - 1, elm_idx - 1) - x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx + 1));
 	}
 }/*device*/ }/*diffusion*/ }/*iki*/
