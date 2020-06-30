8707847e91be5fbd537417563d0c0543de6a4063
Mark Shevelev <mposimba@gmail.com>
2020-06-30 15:39:04 +0300

    boundary growth rate values are duplicated from nearest neighbor

--- WHFI-CUDA-1.1/growth_rate_kernel.cuh
+++ WHFI-CUDA-1.1/growth_rate_kernel.cuh
@@ -16,11 +16,14 @@ namespace iki { namespace whfi { namespace device {
 		iki::table::device::DeviceDataLine<T> gamma
 	) {
 		unsigned idx = threadIdx.x + blockDim.x * blockIdx.x;
-		if (0 == idx || first_moment.size - 1 == idx) { gamma(idx) = T(0.);  return; }
-
+		if (0 == idx || first_moment.size - 1 == idx) return;
+		
 		auto first_moment_derive = T(0.5) * (first_moment(idx + 1) - first_moment(idx - 1)) / vparall_step;
 
 		//\sqrt(\frac{\pi}{2}) = 1.25331414
 		gamma(idx) = -T(1.25331414) * (first_moment_derive - zero_moment(idx) / k_betta(idx)) / dispersion_derive(idx);
+
+		if (1 == idx) { gamma(0) = gamma(idx); }
+		if (first_moment.size - 2 == idx) { gamma(first_moment.size - 1) = gamma(idx);  }
 	}
 }/*device*/ }/*whfi*/ }/*iki*/
