0f6038853aaca81fc9a1542b4dca4fffc14a190d
Mark Shevelev <mposimba@gmail.com>
2020-06-30 17:42:57 +0300

    literal constants explicit type casting

--- WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
+++ WHFI-CUDA-1.1/forward_step_matrix_calculation_kernel.cuh
@@ -21,11 +21,11 @@ namespace iki { namespace diffusion{ namespace device {
 
 		if (0 == row_idx || x_curr.row_count - 1 == row_idx || 0 == elm_idx || x_curr.row_size - 1 == elm_idx) return;
 
-		a(row_idx,elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx-1);
-		b(row_idx, elm_idx) = T(1) + 0.5 * along_r * (along_dfc(row_idx, elm_idx-1) + along_dfc(row_idx, elm_idx));
-		c(row_idx, elm_idx) = -0.5 * along_r * along_dfc(row_idx, elm_idx);
+		a(row_idx, elm_idx) = -T(0.5) * along_r * along_dfc(row_idx, elm_idx - 1);
+		b(row_idx, elm_idx) = T(1) + T(0.5) * along_r * (along_dfc(row_idx, elm_idx - 1) + along_dfc(row_idx, elm_idx));
+		c(row_idx, elm_idx) = -T(0.5) * along_r * along_dfc(row_idx, elm_idx);
 		d(row_idx, elm_idx) = x_curr(row_idx, elm_idx)
-			+ 0.5 * along_r * along_diagonal_discretization(x_curr,along_dfc,row_idx,elm_idx)
+			+ T(0.5) * along_r * along_diagonal_discretization(x_curr,along_dfc,row_idx,elm_idx)
 			+ perp_r * perp_diagonal_discretization(x_curr, perp_dfc, row_idx, elm_idx)
 			+ mixed_r * along_mixed_term_discretization(x_curr,along_mixed_dfc,row_idx,elm_idx)
 			+ mixed_r * perp_mixed_term_discretization(x_curr,perp_mixed_dfc,row_idx,elm_idx);
