43ce40ca2e674990aab4cdeeb28c4ece8cf1b659
Mark Shevelev <mposimba@gmail.com>
2020-07-03 09:55:46 +0300

    vparall_axis extended to the negative growth rate

--- WHFI-CUDA-1.1/MainTest.cu
+++ WHFI-CUDA-1.1/MainTest.cu
@@ -20,8 +20,9 @@ int main() {
 	try {
 		if (true) {
 			auto params = init_parameters(0.95f, 1.0f / 0.95f, 1.f/6.f, -2.f);
-			unsigned vparall_size = 512; unsigned vperp_size = 3072;
-			Axis<float> vparall_axis = construct_vparall_axis<float>(params,make_ZFunc<float>(1.e-5f, 15.f), vparall_size, -12.f, -0.96f);
+			unsigned vparall_size = 1024; unsigned vperp_size = 3072;
+			//Axis<float> vparall_axis = construct_vparall_axis<float>(params,make_ZFunc<float>(1.e-5f, 15.f), vparall_size, -12.f, -0.96f);
+			Axis<float> vparall_axis = { -12.f, 1.e-2f };
 			Axis<float> vperp_axis = { -0.75e-2f, 1.5e-2f };
 
 			//analytical growth rate
@@ -49,8 +50,8 @@ int main() {
 
 			vdf_diffusion<float>(
 				params, Space<float>{vparall_axis, vperp_axis}, vparall_size, vperp_size,
-				1.0e-6f, 2000.f, //amplitude, amplitude time
-				4000, 1.f,  //iterations, dt
+				1.0e-9f, 4000.f, //amplitude, amplitude time
+				2000, 1.f,  //iterations, dt
 				false,         //initial core dfc export
 				false,         //dfc recalculation
 				true, 1000,   //intermidiate growth rate export
