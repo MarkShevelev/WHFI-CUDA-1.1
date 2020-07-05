f5b2d999dd7d0074921a6389d89f883d8f3d8a8b
Mark Shevelev <mposimba@gmail.com>
2020-07-05 18:40:20 +0300

    conservative forward discretization for the mixed terms

--- WHFI-CUDA-1.1/mixed_term_discretization.cuh
+++ WHFI-CUDA-1.1/mixed_term_discretization.cuh
@@ -7,24 +7,48 @@
 namespace iki { namespace diffusion { namespace device {
 	template <typename T>
 	__device__ T along_mixed_term_discretization(table::device::DeviceTable<T> x_curr, table::device::DeviceTable<T> along_mixed_dfc, unsigned row_idx, unsigned elm_idx) {
-		T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx + 1));
-		T central_grad = (x_curr(row_idx + 1, elm_idx) - x_curr(row_idx - 1, elm_idx));
-		T left_grad = (x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx - 1));
+		if (along_mixed_dfc.row_size - 2 == elm_idx) {
+			T left_grad = (x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx - 1));
+			T central_grad = (x_curr(row_idx + 1, elm_idx) - x_curr(row_idx - 1, elm_idx));
+			T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx + 1));
 
-		T right_q = along_mixed_dfc(row_idx, elm_idx) * (3 * right_grad + 6 * central_grad - left_grad);
-		T left_q = along_mixed_dfc(row_idx, elm_idx-1) * (3 * left_grad + 6 * central_grad - right_grad);
+			T left_q = along_mixed_dfc(row_idx, elm_idx - 1) * (3 * left_grad + 6 * central_grad - right_grad);
+			T right_q = along_mixed_dfc(row_idx, elm_idx) * (3 * right_grad + 6 * central_grad - left_grad);
+
+			return (right_q - left_q) / 16;
+		}
+
+		T left_grad = (x_curr(row_idx + 1, elm_idx - 1) - x_curr(row_idx - 1, elm_idx - 1));
+		T central_grad = (x_curr(row_idx + 1, elm_idx) - x_curr(row_idx - 1, elm_idx));
+		T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx + 1));
+		T forward_grad = (x_curr(row_idx + 1, elm_idx + 2) - x_curr(row_idx - 1, elm_idx + 2));
+
+		T left_q = along_mixed_dfc(row_idx, elm_idx - 1) * (3 * left_grad + 6 * central_grad - right_grad);
+		T right_q = along_mixed_dfc(row_idx, elm_idx) * (3 * central_grad + 6 * right_grad - forward_grad);
 
 		return (right_q - left_q) / 16;
 	}
 
 	template <typename T>
 	__device__ T perp_mixed_term_discretization(table::device::DeviceTable<T> x_curr, table::device::DeviceTable<T> perp_mixed_dfc, unsigned row_idx, unsigned elm_idx) {
-		T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx + 1, elm_idx - 1));
-		T central_grad = (x_curr(row_idx, elm_idx + 1) - x_curr(row_idx, elm_idx - 1));
-		T left_grad = (x_curr(row_idx - 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx - 1));
+		if (perp_mixed_dfc.row_count - 2 == row_idx) {
+			T left_grad = (x_curr(row_idx - 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx - 1));
+			T central_grad = (x_curr(row_idx, elm_idx + 1) - x_curr(row_idx, elm_idx - 1));
+			T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx + 1, elm_idx - 1));
 
-		T right_q = perp_mixed_dfc(elm_idx, row_idx) * (3 * right_grad + 6 * central_grad - left_grad);
+			T left_q = perp_mixed_dfc(elm_idx, row_idx - 1) * (3 * left_grad + 6 * central_grad - right_grad);
+			T right_q = perp_mixed_dfc(elm_idx, row_idx) * (3 * right_grad + 6 * central_grad - left_grad);
+
+			return (right_q - left_q) / 16;
+		}
+
+		T left_grad = (x_curr(row_idx - 1, elm_idx + 1) - x_curr(row_idx - 1, elm_idx - 1));
+		T central_grad = (x_curr(row_idx, elm_idx + 1) - x_curr(row_idx, elm_idx - 1));
+		T right_grad = (x_curr(row_idx + 1, elm_idx + 1) - x_curr(row_idx + 1, elm_idx - 1));
+		T forward_grad = (x_curr(row_idx + 2, elm_idx + 1) - x_curr(row_idx + 2, elm_idx - 1));
+
 		T left_q = perp_mixed_dfc(elm_idx, row_idx - 1) * (3 * left_grad + 6 * central_grad - right_grad);
+		T right_q = perp_mixed_dfc(elm_idx, row_idx) * (3 * central_grad + 6 * right_grad - forward_grad);
 
 		return (right_q - left_q) / 16;
 	}
