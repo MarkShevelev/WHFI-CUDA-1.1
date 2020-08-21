1fbf0e10bec9041ea5edc4b83bbfce4f51867b53
Mark Shevelev <mposimba@gmail.com>
2020-08-21 13:59:45 +0300

    two points second order folding for the nonuniform grid added

--- WHFI-CUDA-1.1/data_line_folding.h
+++ WHFI-CUDA-1.1/data_line_folding.h
@@ -4,10 +4,19 @@
 
 namespace iki { namespace whfi {
 	template <typename T>
-	T data_line_folding(table::HostDataLine<T> const &line, T step) {
+	T four_points_on_uniform_grid(table::HostDataLine<T> const &line, T step) {
 		T sum = T(0.); //3/8 formula
 		for (unsigned idx = 1; idx + 3 < line.size; idx += 3) {
 			sum += T(3. / 8.) * step * (line(idx) + T(3.) * line(idx + 1) + T(3.) * line(idx + 2) + line(idx + 3));
+		}
+		return sum;
+	}
+
+	template <typename T>
+	T two_points_on_nonuniform_grid(table::HostDataLine<T> const &val, table::HostDataLine<T> const &arg) {
+		T sum = T(0);
+		for (unsigned idx = 1; idx < val.size; idx += 1) {
+			sum += (val(idx - 1) + val(idx)) / 2 * (arg(idx) - arg(idx - 1));
 		}
 		return sum;
 	}
